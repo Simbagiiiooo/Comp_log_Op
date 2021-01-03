3e4cb5604ca57a9e8c959795a4720543b979e585
Simbagiiiooo <67731418+Simbagiiiooo@users.noreply.github.com>
2021-01-03 14:20:58 +0100

    Add files via upload

---
+++ 22_Comp_Logi_Op/script.js
@@ -0,0 +1,39 @@
+let age = 17;
+let isFemale = false;
+let driverStatus = 'Bob';
+let Name = 'Bram';
+let totalAmount = 100;
+// overal aparte else maken!
+if (totalAmount > 25 && totalAmount <=50) {
+    console.log("U krijgt gratis (vega)bitterballen!");
+} else if (totalAmount > 50 && totalAmount < 100) {
+    console.log("U krijgt gratis een portie nachos");
+} else if (totalAmount >=100) {
+    console.log("U krijgt een fles champagne!!!");
+} else {
+    console.log("Bestel voor meer dan 25 euro en u krijgt een cadeau!");
+}
+
+/*  if (Name == 'Sarah' || Name == 'Bram') {
+    console.log("Jij krijgt een gratis biertje!")
+} else {
+    console.log("Welkom, veel plezier!")
+}
+if (age >= 18 && age <= 25) {
+    console.log("Je krijgt 50% korting!");
+} if (age < 18) {
+    console.log("Jij bent jonger dan 18 Fuck off!!!");
+} else {
+    console.log("Kom maar binnen");
+}
+if (driverStatus =='Bob') {
+    console.log("Jij mag autorijden!");
+} else {
+    console.log("Gij mag nie rijen, gij het gezopen!!");
+}
+if (isFemale) {
+    console.log("Hoeveel kaartjes wilt u?");
+} else {
+    console.log("Een vrouw met een piel, dacht het niet!!");
+}
+  */
